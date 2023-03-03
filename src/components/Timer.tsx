8f5fb830823e0482892e7de96a6fb4ecd7466e7f
Anatoly <anatolyavbme@gmail.com>
2023-03-03 12:20:59 +0200

    CW31 - React Fundamental

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,13 @@
 import React from "react"
 export const Timer:React.FC = () =>{
 const styles: React.CSSProperties ={backgroundColor:"lightblue",fontSize:"2em"};
+setTimeout(tic, 1000);
+const[time,setTime] = React.useState(new Date())
+function tic(){
+   setTime(new Date())
+}   
     return <div>
-        <p style={styles}>{new Date().toTimeString()}</p>
+        <h2 className="CurrentTime">Current Time</h2>
+        <p style={styles}>{time.toLocaleTimeString()}</p>
     </div>
 }
