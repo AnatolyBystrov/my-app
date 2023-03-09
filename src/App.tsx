dc97a369d76cf2997029f86babffaf81eb8943ab
Anatoly <anatolyavbme@gmail.com>
2023-03-09 20:29:41 +0200

    HW32 - React  Fundamentals

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-
+import React from "react";
+import {Timer} from "./components/Timer";
 import './App.css';
-import { Timer } from './components/Timer';
 
-function App() {
-  return <Timer/>
-    
+function App()
+{
+  return  <section className='section'><h2 className='firstCouple'>
+    <div className='firstCountry'> <Timer cityCountry="Toronto" /></div>
+    <div className='secondCountry'> <Timer cityCountry="London" /></div></h2>
+    <h2 className='secondCouple'><div className='thirdCountry'> <Timer cityCountry="Japan" /></div>
+    <div className='fourCountry'><Timer cityCountry="Jerusalem" />
+    </div></h2></section>;
 }
 
 export default App;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,13 +1,48 @@
-import React from "react"
-export const Timer:React.FC = () =>{
-const styles: React.CSSProperties ={backgroundColor:"lightblue",fontSize:"2em"};
-setTimeout(tic, 1000);
-const[time,setTime] = React.useState(new Date())
-function tic(){
-   setTime(new Date())
-}   
-    return <div>
-        <h2 className="CurrentTime">Current Time</h2>
-        <p style={styles}>{time.toLocaleTimeString()}</p>
-    </div>
-}
+import React from "react";
+import timeZones from "../time-zones";
+type Props ={
+    cityCountry:string;
+  }
+  export const Timer:React.FC<Props>=({cityCountry})=>
+  {
+     
+      const styles: React.CSSProperties= {backgroundColor:"lightblue", fontSize:"2em", color: "red",
+      textAlign:"center"}
+      let stylesH2: React.CSSProperties= {fontSize:"2em",color: "red",textAlign:"center"}
+      let stylesH2New: React.CSSProperties= {fontSize:"2em",color: "blue",textAlign:"center"}
+      setTimeout(tic, 1000);
+      const [time, setTime] = React.useState(new Date());
+      function tic()
+      {
+         setTime(new Date())
+      } 
+      const intervalDuration = 10000;
+  
+      React.useEffect(() => {
+        const interval = setInterval(() => {
+          setChanger((styleColor) => styleColor === stylesH2 ? stylesH2New : stylesH2);
+        }, intervalDuration);
+        return () => clearInterval(interval);
+      }, []);
+        
+      let [changer,setChanger] = React.useState(stylesH2);
+     
+        const findIndex = timeZones.findIndex((element)=>
+        {
+         return element.name.split("/")[1] == cityCountry || element.countryName == cityCountry;
+        });
+  
+        function checking (indexForChecking:number)
+        {
+        const res = indexForChecking === -1 ? indexForChecking = 195 : indexForChecking;
+        return  res;
+        }
+        
+        const json = JSON.stringify(timeZones[checking(findIndex)])
+        const jsonObj = JSON.parse(json);
+  
+      return <div>
+      <h2 style ={changer}> Current time {cityCountry}</h2>
+      <p style={styles}>{time.toLocaleTimeString(undefined,{timeZone:jsonObj.name})}</p>
+      </div>
+      }
